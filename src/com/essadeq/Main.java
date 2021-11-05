a72062f33fb8bc46126ea918bf86d65842a6967a
hatim essadeq <hatim.essadeq@gmail.com>
2021-11-05 18:24:32 +0100

    ds/arrays

--- src/com/essadeq/Main.java
+++ src/com/essadeq/Main.java
@@ -14,17 +14,34 @@ public class Main {
     }
 
     void simpleArray() {
-        SimpleArray array = new SimpleArray();
+        System.out.println("SimpleArray 1D: ");
+        SimpleArray array1D = new SimpleArray(5);
+        array1D.add(0, 1);
+        array1D.add(1, "Hatim");
+        array1D.add(2, 3.14);
+        array1D.add(3, 'R');
+        array1D.add(4, true);
+        array1D.print();
+
+        System.out.println("SimpleArray 2D: ");
+        SimpleArray array2D = new SimpleArray(2, 2);
+        array2D.add(0, 0, 1);
+        array2D.add(0, 1, "Hatim");
+        array2D.add(1, 0, 3.14);
+        array2D.add(1, 1, 'R');
+        array2D.print();
     }
 
     void dynamicArray() {
-        DynamicArray<String> array1 = new DynamicArray<String>();
+        DynamicArray<Object> array1 = new DynamicArray<Object>();
         array1.set("Hatim");
-        System.out.println(array1.getSize());
-        array1.set("Rhita");
-        System.out.println(array1.getSize());
-        array1.set("true");
-        System.out.println(array1.getSize());
+        array1.set(1997);
+        array1.set(true);
+        array1.print();
+
+        System.out.println();
+        array1.delete(1);
+        array1.print();
     }
 
 }
